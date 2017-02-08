5538e240e981f31d7758d06402e55819e533543e
akm <akm2000@gmail.com>
2017-02-08 15:50:59 +0900

    Fix migrations/up.sql

--- migrations/up.sql
+++ migrations/up.sql
@@ -9,8 +9,8 @@ CREATE TABLE `pipeline_jobs` (
 CREATE TABLE `pipeline_job_logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pipeline` varchar(255) NOT NULL,
-  `status` int(11) NOT NULL,
   `message_id` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL,
   `publish_time` datetime NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
