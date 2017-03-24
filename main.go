ddfe088c745db849dc547b89ddb02f72f57b1423
akm <akm2000@gmail.com>
2017-03-24 17:49:34 +0900

    Fix constant name

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "blocks-concurrent-subscriber"
 	app.Usage = "github.com/groovenauts/blocks-concurrent-subscriber"
-	app.Version = Version
+	app.Version = VERSION
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
