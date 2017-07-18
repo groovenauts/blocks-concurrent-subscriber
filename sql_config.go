e80bf9c99765f7f959e3e693e1da8ed26980d245
akm <akm2000@gmail.com>
2017-07-18 19:14:04 +0900

    :+1: Rename update_jobs to update-jobs and insert_logs to insert-logs

--- sql_config.go
+++ sql_config.go
@@ -1,6 +1,6 @@
 package main
 
 type SqlConfig struct {
-	InsertLogs string `json:"insert_logs"`
-	UpdateJobs string `json:"update_jobs"`
+	InsertLogs string `json:"insert-logs"`
+	UpdateJobs string `json:"update-jobs"`
 }
